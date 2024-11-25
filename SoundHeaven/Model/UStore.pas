8bf1eaa0e1d40014664102e09bb43831a30c29d4
AGNALDO FRANCISCO DA MOTA <af_mota@hotmail.com>
2024-11-25 12:30:02 -0300

    Remoção de código não pertencente a aplicação

---
+++ SoundHeaven/Model/UStore.pas
@@ -0,0 +1,33 @@
+unit UStore;
+
+interface
+
+type
+  TStore = class
+  private
+    FID: Integer;
+    FTitulo: string;
+    FArtista: string;
+    FDataLancamento: TDateTime;
+    FAtivo: string;
+  public
+    property ID: Integer read FID write FID;
+    property Titulo: string read FTitulo write FTitulo;
+    property Artista: string read FArtista write FArtista;
+    property DataLancamento: TDateTime read FDataLancamento write FDataLancamento;
+    constructor Create(const ATitulo: string; const AArtista: string; const ADataLancamento: TDateTime);
+  end;
+
+implementation
+
+{ TStore }
+
+constructor TStore.Create(const ATitulo, AArtista: string;
+  const ADataLancamento: TDateTime);
+begin
+  FTitulo := ATitulo;
+  FArtista := AArtista;
+  FDataLancamento := ADataLancamento;
+end;
+
+end.
